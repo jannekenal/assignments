e95386d4cd4b7a07eb40b52bc98f64d158b13a2a
Janneke Wolbers <32904213+jannekenal@users.noreply.github.com>
2017-12-19 14:09:04 +0100

    Add files via upload

---
+++ july.php
@@ -0,0 +1,11 @@
+<?php
+function JulyCheck() 
+if (date("F") =='July') {
+    Its July, so it can get really hot.;
+}
+else {
+    It’s not July, so at least we’re not in the peak of the heat.;
+}
+
+print "JulyCheck";
+?>
